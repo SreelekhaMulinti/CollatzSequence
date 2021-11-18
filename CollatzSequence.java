ff5fc65aeb600ccdddb7d8e5b04248a0f82cafa2
SreelekhaMulinti <93986172+SreelekhaMulinti@users.noreply.github.com>
2021-11-18 09:46:33 +0530

    Update CollatzSequence.java

--- CollatzSequence.java
+++ CollatzSequence.java
@@ -3,6 +3,7 @@ public class CollatzSequence{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int operations = 0;
 		while(n != 1){
 			System.out.print(n+" ");
 			//If n is even, n = n/2
@@ -13,7 +14,8 @@ public class CollatzSequence{
 			else{
 				n = 3*n+1;
 			}
+			operations++;
 		}
-		System.out.print(n+" ");
+		System.out.print(n+" "+operations);
 	}
 }
